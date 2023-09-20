576dc3ac9f2c16737e8d4e1ae6022b3f65731c55
Lesliee Sarahi <143286669+LesliSara@users.noreply.github.com>
2023-09-20 16:27:53 -0600

    [EEDIT][TIME][LESLIEE] Scrip terminado con buena presentación

--- time.sh
+++ time.sh
@@ -4,5 +4,21 @@
 
 timedatectl set-local-rtc 1 #Set el reloj as local time
 
-printf "Fecha: " && cat /sys/class/rtc/rtc0/date
-printf "Hora: " && cat /sys/class/rtc/rtc0/time
+printf "\n"
+echo -e "\e[40m\e[32m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\e[0m"
+echo -e "\e[40m\e[32m░░                                                                             ░░\e[0m"
+echo -e "\e[40m\e[32m░░          ║██████  ║██   ║█████   ║██     ╔╝███  ║█████   ╔═╝█████           ░░\e[0m"
+echo -e "\e[40m\e[32m░░           ╔╝██   ╔╝██  ╔╝██     ╔╝███   ╔╝███  ╔╝██  ██ ╔╝██   ╔╝██         ░░\e[0m"
+echo -e "\e[40m\e[32m░░           ║██    ║██   ║█████   ║██ ██ ║██ ██  ║██████  ║██    ║██          ░░\e[0m"
+echo -e "\e[40m\e[32m░░          ╔╝██   ╔╝██  ╔╝██     ╔╝██  ███ ╔╝██ ╔╝██      ║██    ║██          ░░\e[0m"
+echo -e "\e[40m\e[32m░░          ║██    ║██   ║█████   ║██    █  ║██  ║██       ╚══█████            ░░\e[0m"
+echo -e "\e[40m\e[32m░░                                                                             ░░\e[0m"
+echo -e "\e[40m\e[32m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\e[0m"
+echo -e "\e[40m\e[32m                                                                                 \e[0m"
+
+date=$(cat /sys/class/rtc/rtc0/date)
+hora=$(cat /sys/class/rtc/rtc0/time)
+
+echo -e "\e[40m\e[32m\033[1mFECHA: $date                                                                \033[0m\e[0m" 
+echo -e "\e[40m\e[32m\033[1mHORA: $hora                                                                   \033[0m\e[0m" 
+printf "\n"
