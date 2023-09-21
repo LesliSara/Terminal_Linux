49c76c43c61f7118885cf830aacbe5f6b364d1a7
Lesliee Sarahi <143286669+LesliSara@users.noreply.github.com>
2023-09-20 22:33:30 -0600

    [EDIT][TIME][LESLIEE] Se movió una línea

--- time.sh
+++ time.sh
@@ -2,7 +2,6 @@
 
 #Ruta /sys/class/rtc/rtc contiene archivos de fecha y hora. Sin embargo, la hora está establecida en utc
 
-timedatectl set-local-rtc 1 #Set el reloj as local time
 
 printf "\n"
 echo -e "\e[40m\e[32m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\e[0m"
@@ -16,8 +15,11 @@ echo -e "\e[40m\e[32m░░                                                     
 echo -e "\e[40m\e[32m░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\e[0m"
 echo -e "\e[40m\e[32m                                                                                 \e[0m"
 
+timedatectl set-local-rtc 1 #Set el reloj as local time
+
 date=$(cat /sys/class/rtc/rtc0/date)
 hora=$(cat /sys/class/rtc/rtc0/time)
+
 
 echo -e "\e[40m\e[32m\033[1mFECHA: $date                                                                \033[0m\e[0m" 
 echo -e "\e[40m\e[32m\033[1mHORA: $hora                                                                   \033[0m\e[0m" 
